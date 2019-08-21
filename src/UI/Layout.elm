23922837f25190362f4eef6b7e991373b746e15c
Andreas Lundkvist <andreas@galdiuz.com>
2019-08-21 21:01:51 +0200

    dev: Feats

--- src/UI/Layout.elm
+++ src/UI/Layout.elm
@@ -1,4 +1,4 @@
-module UI.Layout exposing (box)
+module UI.Layout exposing (box, scrollBox)
 
 import Element as El exposing (Element)
 import Element.Background as Background
@@ -15,5 +15,20 @@ box inner =
         , El.spacing 5
         , El.width El.fill
         , El.height El.fill
+        ]
+        inner
+
+
+scrollBox : List (Element msg) -> Element msg
+scrollBox inner =
+    El.column
+        [ Background.color <| El.rgb 0.9 0.9 0.9
+        , Border.width 1
+        , Border.rounded 2
+        , El.padding 5
+        , El.spacing 5
+        , El.width El.fill
+        , El.height El.fill
+        , El.scrollbarY
         ]
         inner
--- src/View/Feat.elm
+++ src/View/Feat.elm
@@ -3,11 +3,14 @@ module View.Feat exposing (render)
 import Dict
 
 import Element as El exposing (Element)
+import Element.Border as Border
 
 import App.Msg as Msg exposing (Msg)
 import App.State exposing (State)
 import Pathfinder2.Data as Data
-import UI.ChooseOne
+import UI.ButtonGrid
+import UI.Layout
+import UI.Text
 
 
 render : State -> Int -> String -> Element Msg
@@ -15,18 +18,64 @@ render state level tag =
     El.column
         [ El.alignTop
         , El.width El.fill
+        , El.height El.fill
+        , El.scrollbarY
+        , El.spacing 10
         ]
-        [ UI.ChooseOne.render
-            { all =
-                state.data.feats
-                    |> Data.filterFeatTag tag
-                    |> Dict.values
-            , available =
-                state.data.feats
-                    |> Data.filterFeatTag tag
-                    |> Dict.values
-            , selected = Nothing
-            , onChange = \_ -> Msg.NoOp
-            , toString = .name
-            }
+        [ El.el
+            [ El.width El.fill
+            ]
+            <| UI.Layout.box
+                [ El.text "Search"
+                ]
+        , El.el
+            [ El.height <| El.fill
+            , El.scrollbarY
+            ]
+            <| UI.Layout.scrollBox
+                [ renderList state level tag
+                ]
         ]
+
+
+renderList : State -> Int -> String -> Element Msg
+renderList state level tag =
+    UI.ButtonGrid.renderChooseOne
+        { all =
+            state.data.feats
+                |> Data.filterFeatTag tag
+                |> Dict.values
+        , available =
+            state.data.feats
+                |> Data.filterFeatTag tag
+                |> Dict.values
+        , selected = Nothing
+        , onChange = \_ -> Msg.NoOp
+        , columns =
+            [ \feat ->
+                El.column
+                    [ El.spacing 5
+                    ]
+                    [ UI.Text.header3 feat.name
+                    , El.row
+                        [ El.spacing 5
+                        ]
+                        <| List.map
+                            ( El.el
+                                [ Border.width 1
+                                , El.padding 2
+                                ]
+                                << El.text
+                            )
+                            feat.tags
+                    ]
+            , \feat ->
+                El.el
+                    [ El.alignRight
+                    , El.centerY
+                    , El.padding 5
+                    , Border.width 1
+                    ]
+                    <| El.text <| String.fromInt <| feat.level
+            ]
+        }
